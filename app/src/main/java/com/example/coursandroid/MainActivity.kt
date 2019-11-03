099e38ffe7b4c3899bf7c65b0bf198957f7d6981
Maxime <pocard.maxime@gmail.com>
2019-11-03 20:13:36 +0100

    Removed margin

--- app/src/main/java/com/example/coursandroid/MainActivity.kt
+++ app/src/main/java/com/example/coursandroid/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.coursandroid
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import com.example.coursandroid.game.*
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -49,8 +52,17 @@ class MainActivity : AppCompatActivity(), GameFragment.OnListFragmentInteraction
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Add margin of the status bar for maximum compatibilities
-        mainLayout.setMargins(0, getStatusBarHeight(), 0 , 0)
+
+        if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
+            setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true)
+        }
+        if (Build.VERSION.SDK_INT >= 19) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        }
+        if (Build.VERSION.SDK_INT >= 21) {
+            setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
+            window.statusBarColor = Color.TRANSPARENT
+        }
 
         // Load the game list fragment
         val transaction = supportFragmentManager.beginTransaction()
@@ -64,34 +76,15 @@ class MainActivity : AppCompatActivity(), GameFragment.OnListFragmentInteraction
         cpt = pref.getInt(PREF_NAME, 0)
     }
 
-    // Add method in view to dynamically change margin
-    fun View.setMargins(
-        left: Int? = null,
-        top: Int? = null,
-        right: Int? = null,
-        bottom: Int? = null
-    ) {
-        val lp = layoutParams as? ViewGroup.MarginLayoutParams
-            ?: return
-
-        lp.setMargins(
-            left ?: lp.leftMargin,
-            top ?: lp.topMargin,
-            right ?: lp.rightMargin,
-            bottom ?: lp.rightMargin
-        )
-
-        layoutParams = lp
-    }
-
-    // Method used to get the height of the status bar
-    private fun getStatusBarHeight(): Int {
-        var result: Int = 0
-        val resourceId: Int = getResources().getIdentifier("status_bar_height", "dimen", "android")
-        if (resourceId > 0) {
-            result = getResources().getDimensionPixelSize(resourceId)
+    private fun setWindowFlag(bits: Int, on: Boolean) {
+        val win = window
+        val winParams = win.attributes
+        if (on) {
+            winParams.flags = winParams.flags or bits
+        } else {
+            winParams.flags = winParams.flags and bits.inv()
         }
-        return result;
+        win.attributes = winParams
     }
 
     // When the application stop, save the counter in the shared preferences
